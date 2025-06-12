20c04d9757b51ed831ec205c77b6f4596333e621
Emile Fugulin <code@efugulin.com>
2025-06-12 09:53:21 -0400

    Renamed passthrough

--- src/passthrough.rs
+++ src/passthrough.rs
@@ -1,7 +1,7 @@
 /// A trait to set passthrough on nested buffered readers.
 ///
 /// This allows for disabling the buffer even if the reader is
-/// nested other buffered readers.
+/// nested within other buffered readers.
 pub trait AsyncBufPassthrough {
     /// Set the buffered reader to start or stop acting as a passthrough for the
     /// underlying reader.
@@ -12,26 +12,26 @@ pub trait AsyncBufPassthrough {
     ///
     /// This is useful when you need to peek data only at the beginning of a stream
     /// to determine how to parse it, but then don't need to buffer the rest of the stream.
-    fn passthrough(&mut self, passthrough: bool);
+    fn passthrough(&mut self, enabled: bool);
 }
 
 #[cfg(feature = "tokio-rustls")]
 impl<IO: AsyncBufPassthrough> AsyncBufPassthrough for tokio_rustls::server::TlsStream<IO> {
-    fn passthrough(&mut self, passthrough: bool) {
-        self.get_mut().0.passthrough(passthrough);
+    fn passthrough(&mut self, enabled: bool) {
+        self.get_mut().0.passthrough(enabled);
     }
 }
 
 #[cfg(feature = "tokio-rustls")]
 impl<IO: AsyncBufPassthrough> AsyncBufPassthrough for tokio_rustls::client::TlsStream<IO> {
-    fn passthrough(&mut self, passthrough: bool) {
-        self.get_mut().0.passthrough(passthrough);
+    fn passthrough(&mut self, enabled: bool) {
+        self.get_mut().0.passthrough(enabled);
     }
 }
 
 #[cfg(feature = "tokio-openssl")]
 impl<IO: AsyncBufPassthrough> AsyncBufPassthrough for tokio_openssl::SslStream<IO> {
-    fn passthrough(&mut self, passthrough: bool) {
-        self.get_mut().passthrough(passthrough);
+    fn passthrough(&mut self, enabled: bool) {
+        self.get_mut().passthrough(enabled);
     }
 }
